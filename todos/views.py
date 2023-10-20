3f405492cf730507beaf1ffa88e7281c1ceba87a
kemuelkesley <kemuelkesleytkd@outlook.com>
2023-10-20 20:54:59 -0300

    Criado o arquivo todo_form.html.

--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Todo
 
 
-def todo_list(request):
-    # nome = "Rafael"
-    # alunos = ["Kemuel kesley", "Ariel Sardinha", "Anna Beatriz"]
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+# def todo_list(request):
+#     nome = "Rafael"
+#     alunos = ["Kemuel kesley", "Ariel Sardinha", "Anna Beatriz"]
+#     todos = Todo.objects.all()
+#     return render(request, "todos/todo_list.html", {"todos": todos})
+
+
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
